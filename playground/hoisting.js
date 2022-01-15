9b1a5cb18943edb5d723ffadc4b5b96a5505be24
Pablo Rosa <panicoro770@gmail.com>
2022-01-15 15:44:36 -0300

    Hosting

---
+++ playground/hoisting.js
@@ -0,0 +1,14 @@
+console.log(age);
+// console.log(cool);
+var age = 12;
+
+sayHi();
+
+function sayHi() {
+  console.log('Hi');
+  console.log(add(6, 3));
+}
+
+function add(a, b) {
+  return a + b;
+}
