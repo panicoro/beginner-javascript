16c5714e03edf18d5b45622208a190af1b5df1bf
Pablo Rosa <panicoro770@gmail.com>
2022-02-12 18:00:14 -0300

    Adding custom class

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,4 +1,4 @@
-const p = document.querySelector('p');
+/* const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
@@ -21,3 +21,40 @@ console.log(pizzaList.textContent);
 // pizzaList.textContent = `${`${pizzaList.textContent} 🍕`}`;
 pizzaList.insertAdjacentText('afterbegin', '🍕');
 pizzaList.insertAdjacentText('beforend', '🍕');
+
+*/
+
+// Classes
+
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
+
+pic.alt = 'Nice beach'; // setter
+console.log(pic.alt); // getter
+console.log(pic.naturalWidth);
+
+pic.width = 200;
+
+/* window.addEventListener('load', () => {
+  console.log(pic.naturalWidth);
+});
+*/
+
+/* pic.setAttribute('wes-os-cool', 'REALLY CUTE PUP');
+console.log(pic.getAttribute('alt'));
+*/
+
+const custom = document.querySelector('.custom');
+console.log(custom.dataset);
+
+document.addEventListener('click', () => {
+  alert(`Welcome ${custom.dataset.name} ${custom.dataset.last}`);
+});
