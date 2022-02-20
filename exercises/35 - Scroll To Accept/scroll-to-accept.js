cba79da82a0037011676eb276951ab5561253a06
Pablo Rosa <panicoro770@gmail.com>
2022-02-20 02:24:12 -0300

    Adding Scroll To Accept exercise

---
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -0,0 +1,19 @@
+const terms = document.querySelector('.terms-and-conditions');
+// const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+// eslint-disable-next-line no-use-before-define
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+ob.observe(terms.lastElementChild);
